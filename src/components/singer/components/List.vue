90bc1217d27ee21d0b5cd4d361685127bfea7f12
张银 <zy1017730413@gmail.com>
2021-01-29 18:02:05 +0800

    初始化

---
+++ src/components/singer/components/List.vue
@@ -0,0 +1,27 @@
+<template>
+  <scroll>
+    <div class="list">
+      <div class="list-content">
+        <div class="area">
+          <div class="title">热门</div>
+          <div class="item">
+            <img src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+            <p>薛之谦</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </scroll>
+</template>
+
+<script>
+import scroll from 'base/scroll/scroll.vue'
+export default {
+  components: { scroll },
+
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
---
+++ src/components/singer/singer.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>singer</div>
+</template>
+
+<script>
+import { getSingerList } from "src/api/singer";
+import { ERR_OK } from "src/api/config";
+export default {
+  name: "Singer",
+  data() {
+    return {
+      singers: [],
+    }
+  },
+  created() {
+    this._getSingerList();
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then(res => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list;
+          console.log(this.singers);
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
