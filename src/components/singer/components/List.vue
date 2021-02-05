58422ac07fd24d75fffcf9af1470423222c914e8
张银 <zy1017730413@gmail.com>
2021-02-05 18:34:44 +0800

    (歌手数据分类)

--- src/components/singer/components/List.vue
+++ src/components/singer/components/List.vue
@@ -1,27 +1,59 @@
 <template>
-  <scroll>
+  <Scroll>
     <div class="list">
       <div class="list-content">
-        <div class="area">
+        <div class="area" v-for="item in singers" :key="item.Fsinger_id">
           <div class="title">热门</div>
           <div class="item">
-            <img src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000" alt="">
+            <img
+              src="https://y.gtimg.cn/music/photo_new/T001R300x300M000002J4UUk29y8BY.jpg?max_age=2592000"
+              alt=""
+            />
             <p>薛之谦</p>
           </div>
         </div>
       </div>
     </div>
-  </scroll>
+  </Scroll>
 </template>
 
 <script>
-import scroll from 'base/scroll/scroll.vue'
+import Scroll from "base/scroll/scroll.vue";
 export default {
-  components: { scroll },
-
-}
+  components: { Scroll },
+  props: {
+    singers: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  computed: {
+    
+  }
+};
 </script>
 
 <style lang="stylus" scoped>
-
+.area
+  .title
+    height: 30px
+    line-height: 30px
+    padding-left: 20px
+    color: hsla(0,0%,100%,.5)
+    font-size: 12px
+    background: #333
+  .item
+    display: flex
+    align-items: center
+    padding: 20px 0 0 30px
+    img
+      width 50px
+      height 50px
+      border-radius 50%
+    p
+      color: hsla(0,0%,100%,.5)
+      font-size: 14px
+      margin-left: 20px
 </style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,16 +1,21 @@
 <template>
-  <div>singer</div>
+  <div>
+    <List :singers="singers"></List>
+  </div>
 </template>
 
 <script>
 import { getSingerList } from "src/api/singer";
 import { ERR_OK } from "src/api/config";
+import List from "./components/List";
+const HOT_NAME = "热门城市";
+const HOT_SINGER_LEN = 10;
 export default {
   name: "Singer",
   data() {
     return {
-      singers: [],
-    }
+      singers: []
+    };
   },
   created() {
     this._getSingerList();
@@ -20,12 +25,47 @@ export default {
       getSingerList().then(res => {
         if (res.code === ERR_OK) {
           this.singers = res.data.list;
-          console.log(this.singers);
+          console.log(this._normalize(this.singers));
         }
       });
+    },
+    _normalize(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      };
+      list.forEach((item, index) => {
+        if (index <= HOT_SINGER_LEN) {
+          map.hot.items.push({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid,
+            avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+          });
+        }
+        const key = item.Findex;
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          };
+        }
+        map[key].items.push({
+          name: item.Fsinger_name,
+          id: item.Fsinger_mid,
+          avatar: `https://y.gtimg.cn/music/photo_new/T001R300x300M000${item.Fsinger_mid}.jpg?max_age=2592000`
+        });
+      });
+      return map;
     }
+  },
+  components: {
+    List
   }
 };
 </script>
 
-<style></style>
+<style>
+  
+</style> 
